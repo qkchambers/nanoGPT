c8c6e494208b7a1387b971a3c811815a9533e686
Work <work@QBook-Pro.local>
2025-06-11 14:25:13 -0300

    Made changes for char model

--- config/train_shakespeare_char.py
+++ config/train_shakespeare_char.py
@@ -2,7 +2,7 @@
 # good for debugging and playing on macbooks and such
 
 out_dir = 'out-shakespeare-char'
-eval_interval = 2 # keep frequent because we'll overfit
+eval_interval = 20 # keep frequent because we'll overfit
 eval_iters = 100
 log_interval = 10 # don't print too too often
 
@@ -25,12 +25,12 @@ n_embd = 512
 dropout = 0.2
 
 learning_rate = 3e-4 # with baby networks can afford to go a bit higher
-max_iters = 1000
+max_iters = 500
 lr_decay_iters = 5000 # make equal to max_iters usually
 min_lr = 1e-4 # learning_rate / 10 usually
 beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
 
-device = 'cuda'
+device = 'mps'
 warmup_iters = 100 # not super necessary potentially
 
 # on macbook also add
